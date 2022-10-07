21d834e89bcc9e59ff6ad0b9e7bc4d2b5d2b5045
Danielrl004 <danielrl@hawaii.edu>
2022-10-07 13:57:59 -1000

    lab 11

--- Lab11/Ex1.js
+++ Lab11/Ex1.js
@@ -1,5 +1,11 @@
 step1=99;
 step2=parseInt(step1/4);
 step3 = step2 + step1;
-step4 = 13; // Not Jan, so look at month before on table
-step6 = step4 + step3;
+step4 = 3; // Not Jan, so look at month before on table
+step6 = step4 + step3; // Add month number
+step7 = step6 + 13; // Add date #: March 13
+if(step7 > 7) { 
+    remainder = (step7%7);
+    console.log(remainder); // 6 = Saturday
+};
+
